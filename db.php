16be140c4cf595c0df281c8683abfbe86c7fcbcd
Aparicio Jeric <apariciojeric@gmail.com>
2024-11-14 19:34:33 +0800

    Added database connection and create functionality

---
+++ db.php
@@ -0,0 +1,25 @@
+<?php
+
+$host = 'localhost';
+$user = 'Jeric';
+$pass = '';
+$db = 'final_activity';
+$table = 'table_activity';
+try {
+   $pdo = new PDO("mysql:host=$host", $user, $pass);
+   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTIO);
+   
+   $pdo->exec("CREATE DATABASE IF NOT EXISTS `$db`");
+   $pdo->exec("USE `$db`");
+   
+   $sql = "CREATE TABLE IF NOT EXISTS $table (id INT AUTO_INCREMENT PRIMARY KEY, task VARCHAR(100) NOT NULL, date VARCHAR(10) NOT NULL)";
+   $pdo->exec($sql);
+   
+   echo "Database and Table Created";
+   
+} catch (PDOException $e){
+    echo "Connection failed" .$e->getMessage();
+    
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ToDoList</title>
+    </head>
+    <body>
+        <form action = "" method = "">
+            <h2>To Do List Application</h2>
+            
+            <fieldset>
+                <legend>Create Tasks</legend>
+                
+                <label for = "tasks">Input your tasks</label><br>
+                <input type = "text" name = "task" required><br><br>
+                
+                <label for = "dates">Select Date</label><br>
+                <input type = "date" name = "date"><br><br>
+                
+                <button type="submit">Add task</button>
+            </fieldset>
+        </form>
+    </body>
+</html>
